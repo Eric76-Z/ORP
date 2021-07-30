741e9794368244d19e957f12e7a8742567c763ac
hasaii1 <hasaii1@outlook.com>
2021-07-30 17:53:58 +0800

    完成了备份整理，输出备份情况报告的功能

--- robprogram.py
+++ robprogram.py
@@ -9,10 +9,12 @@ import xlwt
 from xlutils.copy import copy
 
 from pathmap import pathmap
+
 # 预设变量
-BASE_PATH = 'D:\\rob'
-PATHRB = 'D:\\rob\\old'  #原备份所在文件夹
-PATH_EXPORT = 'D:\\rob\\new'  #重整后文件夹位置
+BASE_PATH = 'F:\\New folder'
+PATHRB = BASE_PATH + '\\' + 'old'  # 原备份所在文件夹
+PATH_EXPORT = BASE_PATH + '\\' + 'new'  # 重整后文件夹位置
+LOG_FILE_NAME = 'log.txt'
 
 buStandard = {
     'filepath': BASE_PATH,
@@ -20,6 +22,22 @@ buStandard = {
 }
 # 全局变量
 targetData = {}
+isAll = False  # True:全名对比， False后七位对比，eg:S240R01
+
+# 表格
+SHEET_NAME = 'robot'
+COMPARE_COL = 5
+COMMIT_COL = 6
+TIME_COL = 7
+Lv3 = 4
+Lv2 = 3
+Lv1 = 2
+
+# 总结
+TOTAL_FILES = 0
+DEAL_FILES = 0
+ERR_FILES = 0
+
 
 # RobotDatas = {}
 # mainData = []
@@ -39,87 +57,123 @@ targetData = {}
 # ExclePath = '机器人Rdc数据表V4.xls'
 
 
+def logWrite(controllername, msg):
+    log = open(BASE_PATH + '\\' + LOG_FILE_NAME, 'a')
+    log.write(controllername + '[' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ']' + ' :' + msg + '\r\n')
+    log.close()
+
+
+def logWriteTitle(msg):
+    log = open(BASE_PATH + '\\' + LOG_FILE_NAME, 'a')
+    log.write('==============================' + msg + '==============================' + '\r\n')
+    log.close()
+
+
 # 获取zip格式文件路径路径
 def getZipInfo():
+    global TOTAL_FILES
+
+    global ERR_FILES
     for root, dirs, files in os.walk(PATHRB):
         for name in files:
+            TOTAL_FILES = TOTAL_FILES + 1
             if name.endswith('.zip'):
                 originpath = os.path.join(root, name)
                 controllername = name.split('.zip')[0]
-                # print(controllername[0][-7:])
-                workstationname = controllername[-7:]  # 截取的 eg.k2a3a131s460r04 后7位
-                targetData[controllername] = {}
-                targetData[controllername]['originpath'] = originpath
-                targetData[controllername]['controller'] = controllername
-                targetData[controllername]['workstation'] = workstationname
+                workstationname = controllername[-7:].upper()  # 截取的 eg.k2a3a131s460r04 后7位
+                # print(workstationname)
+                if isAll == True:
+                    compare_name = controllername
+                else:
+                    compare_name = workstationname
+                targetData[compare_name] = {}
+                targetData[compare_name]['originpath'] = originpath
+                targetData[compare_name]['controller'] = compare_name
+                targetData[compare_name]['workstation'] = workstationname
                 if controllername[0:2] == 'k1':
-                    targetData[controllername]['Lv1'] = 'CPH2.1'
+                    targetData[compare_name]['Lv1'] = 'CPH2.1'
                 elif controllername[0:2] == 'k2':
-                    targetData[controllername]['Lv1'] = 'CPH2.2'
+                    targetData[compare_name]['Lv1'] = 'CPH2.2'
+                elif controllername[0:2] == 'k3':
+                    targetData[compare_name]['Lv1'] = 'CPH2.1'
+                    logWrite(controllername, '一级地点为k3')
                 else:
-                    targetData[controllername]['errmsg'] = '没有对应一级地点'
-                    raise ('没有对应一级地点')
-                if pathmap[controllername[2:6]]:
-                    targetData[controllername]['Lv2'] = pathmap[controllername[2:6]]['Lv2']
-                    targetData[controllername]['Lv3'] = pathmap[controllername[2:6]]['Lv3']
+                    # print(compare_name)
+                    # targetData[compare_name]['errmsg'] = '没有对应一级地点'
+                    ERR_FILES = ERR_FILES + 1
+                    logWrite(controllername, '没有对应一级地点')
+                    continue
+                if controllername[2:6] in pathmap:
+                    targetData[compare_name]['Lv2'] = pathmap[controllername[2:6]]['Lv2']
+                    targetData[compare_name]['Lv3'] = pathmap[controllername[2:6]]['Lv3']
                 else:
-                    targetData[controllername]['errmsg'] = '找不到对应区域'
-                    raise (controllername + '找不到对应区域')
-                newpath = PATH_EXPORT + '\\' + targetData[controllername]['Lv1'] + '\\' + targetData[controllername][
-                    'Lv2'] + '\\' + targetData[controllername]['Lv3']
-                targetData[controllername]['newpath'] = newpath
+                    logWrite(controllername, '找不到对应区域')
+                    ERR_FILES = ERR_FILES + 1
+                    continue
+                    # targetData[compare_name]['errmsg'] = '找不到对应区域'
+                    # raise (compare_name + '找不到对应区域')
+                newpath = PATH_EXPORT + '\\' + targetData[compare_name]['Lv1'] + '\\' + targetData[compare_name][
+                    'Lv2'] + '\\' + targetData[compare_name]['Lv3']
+                targetData[compare_name]['newpath'] = newpath
 
                 # 移动重整文件
-                folder = os.path.exists(targetData[controllername]['newpath'])
+                folder = os.path.exists(targetData[compare_name]['newpath'])
                 if not folder:  # 判断是否存在文件夹如果不存在则创建为文件夹
-                    os.makedirs(targetData[controllername]['newpath'])  # makedirs 创建文件时如果路径不存在会创建这个路径
+                    os.makedirs(targetData[compare_name]['newpath'])  # makedirs 创建文件时如果路径不存在会创建这个路径
                 else:
                     pass
-                if not os.path.exists(targetData[controllername]['newpath'] + '\\' + name):
-                    print(targetData[controllername]['newpath'] + '\\' + controllername)
-                    # shutil.move(targetData[controllername]['originpath'], targetData[controllername]['newpath'])
-                    shutil.copy2(targetData[controllername]['originpath'], targetData[controllername]['newpath'])
+                if not os.path.exists(targetData[compare_name]['newpath'] + '\\' + name):
+                    # print(targetData[compare_name]['newpath'] + '\\' + compare_name)
+                    # shutil.move(targetData[compare_name]['originpath'], targetData[compare_name]['newpath'])
+                    shutil.copy2(targetData[compare_name]['originpath'], targetData[compare_name]['newpath'])
                 else:
-                    # shutil.move(targetData[controllername]['originpath'],
-                    #             targetData[controllername]['newpath'] + '\\' + name + '副本')
-                    shutil.copy2(targetData[controllername]['originpath'],
-                                 targetData[controllername]['newpath'] + '\\' + name + '副本')
-
-
-# def mapControllername(controllername):
-#
-#
-#         # 写到这
+                    # shutil.move(targetData[compare_name]['originpath'],
+                    #             targetData[compare_name]['newpath'] + '\\' + name + '副本')
+                    # shutil.copy2(targetData[compare_name]['originpath'],
+                    #              targetData[compare_name]['newpath'] + '\\' + name + '副本')
+                    continue
 
 
 def backupState():
-
     standard_filepath = buStandard['filepath'] + '\\' + buStandard['filename']
     book_rd = xlrd.open_workbook(standard_filepath, formatting_info=True)
     sheet_rd = book_rd.sheet_by_index(0)
-
     book_wt = copy(book_rd)
-    worksheet_name = 'rob'
-    sheet_wt = book_wt.get_sheet(worksheet_name)
+    sheet_wt = book_wt.get_sheet(SHEET_NAME)
     nrows = sheet_rd.nrows
+    nrows_compare = nrows
+    global DEAL_FILES
     for root, dirs, files in os.walk(PATH_EXPORT):
         for name in files:
+            DEAL_FILES = DEAL_FILES + 1
             controllername = name.split('.zip')[0]
-
+            workstationname = controllername[-7:].upper()  # 截取的 eg.k2a3a131s460r04 后7位
+            if isAll == True:
+                compare_name = controllername
+            else:
+                compare_name = workstationname
             flag = True
+
             for i in range(nrows):
-                lng = sheet_rd.cell(i - 1, 1).value
-                if lng == controllername:
-                    sheet_wt.write(i, 3, label='已备份')
-                    sheet_wt.write(i, 4, label=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                lng = sheet_rd.cell(i - 1, COMPARE_COL).value
+                if lng == compare_name:
+                    sheet_wt.write(i - 1, COMMIT_COL, label='已备份')
+                    sheet_wt.write(i - 1, TIME_COL, label=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
                     flag = False
                     break
             if flag == True:
-                nrows_new = sheet_rd.nrows  # 行
-                print(nrows_new)
-                sheet_wt.write(nrows_new, 1, controllername)
-                sheet_wt.write(nrows_new, 3, label='新工位')
-                sheet_wt.write(nrows_new, 4, label=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                sheet_wt.write(nrows_compare, COMPARE_COL - 3, targetData[compare_name]['Lv1'])
+                sheet_wt.write(nrows_compare, COMPARE_COL - 2, pathmap[controllername[2:6]]['Lv2'])
+                sheet_wt.write(nrows_compare, COMPARE_COL - 1, pathmap[controllername[2:6]]['Lv3'])
+                sheet_wt.write(nrows_compare, COMPARE_COL, compare_name)
+                sheet_wt.write(nrows_compare, COMMIT_COL, label='新工位')
+                sheet_wt.write(nrows_compare, TIME_COL, label=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                nrows_compare = nrows_compare + 1
+    logWriteTitle('总结')
+    log = open(BASE_PATH + '\\' + LOG_FILE_NAME, 'a')
+    log.write('备份总数: ' + str(TOTAL_FILES) + '        已处理: ' + str(DEAL_FILES) + '         异常: ' + str(ERR_FILES) + '\r\n')
+    log.close()
+    logWriteTitle('end')
     book_wt.save(BASE_PATH + '\\' + time.strftime("%Y%m%d", time.localtime()) + buStandard['filename'])
 
 
@@ -213,8 +267,7 @@ def backupState():
 
 
 def main():
-    # age = input('输入参数：')
-    # print(age)
+    logWriteTitle('start')
     # 1.获取路径
     getZipInfo()
     #
